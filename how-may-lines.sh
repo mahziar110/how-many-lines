36c53d0b8cb651b467a8909c16b125c33775b481
Mahziar Eghdami <mahziar.jordan@gmail.com>
2024-03-30 13:00:08 +0330

    [BUGFIX]

--- how-may-lines.sh
+++ how-may-lines.sh
@@ -3,7 +3,6 @@
 # Function to get lines changed based on time interval
 get_lines_changed() {
     local since_param=""
-    
     case "$1" in
         -d) since_param="midnight";;
         -w) since_param="last week";;
@@ -12,11 +11,9 @@ get_lines_changed() {
         *) echo "Invalid argument. Please use -d, -w, -m, or -y."; exit 1;;
     esac
 
-    readarray -t lines_changed < <(git log --since="$since_param" --numstat --pretty=tformat: | awk '{if ($1 > 0 || $2 > 0) print $1; print $2}' | awk '{sum+=$1; print sum}')
- 
-    insertions=${lines_changed[0]}
-    deletions=${lines_changed[1]}
-    total_lines_changed=${lines_changed[2]}
+    readarray -t lines_changed < <(git log --since="$since_param" --numstat --pretty=tformat: | awk '{if ($1 > 0 || $2 > 0) added+=$1; deleted+=$2} END {print added, deleted, added+deleted}')
+
+    IFS=' ' read -r insertions deletions total_lines_changed <<< "${lines_changed[0]}"
 
     echo "Total insertions: $insertions"
     echo "Total deletions: $deletions"
@@ -29,4 +26,3 @@ if [ $# -eq 0 ]; then
 else
     get_lines_changed $1
 fi
-
